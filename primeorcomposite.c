430a27a9a9705ad48a53ac51c4129a71f1761579
karri sowmya <93367820+karrisowmya@users.noreply.github.com>
2021-12-03 12:37:35 +0530

    Update primeorcomposite.c

--- primeorcomposite.c
+++ primeorcomposite.c
@@ -1,3 +1,15 @@
+/*ALGORITHM TO FIND WHETHER A NUMBER IS PRIME OR COMPOSITE
+Step-1:Start
+Step-2:Accept a positive integer 'n'(number greater than 2)
+Step-3:Initialise 'counter' with value 2
+Step-4:Divide 'n' with the 'counter'
+Step-5:If the remainder is 0 display that 'n' is composite and stop the algorithm
+Step-6:Else increment the 'counter' and repeat from step 4 until the 'counter' value is less than 'n'
+Step-7:After completion of all the steps display that a number 'n' is prime
+Step-8:stop */
+
+
+
 #include <iostream>
 #include <string>
 
@@ -34,3 +46,8 @@ int main()
 
    return 0;
 }
+
+
+/*Learnings
+1.Learned to find whether a number is prime or composite
+2.while loop is used in this program */
