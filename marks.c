7711ed6e048616d1fb9fda3e922bd1e234fdde9f
karri sowmya <93367820+karrisowmya@users.noreply.github.com>
2021-12-03 12:47:10 +0530

    Update marks.c

--- marks.c
+++ marks.c
@@ -1,3 +1,14 @@
+/*ALGORITHM TO FIND TOTAL,AVERAGE,PERCENTAGE OF 5 SUBJECT MARKS
+Step-1:Start
+Step-2:Enter the marks of telugu,hindi,english,maths,physics
+Step-3:Calculate sum of all subjects total=telugu+hindi+english+maths+physics
+Step-4:Divide sum of all subjects by total number of subjects average=total/5
+Step-5:Calculate percentage using percentage=(total/500)*100
+Step-5:Finally print values of total,average,percentage
+Step-6:Stop */
+
+
+
 #include<stdio.h>
 int main()
 {
