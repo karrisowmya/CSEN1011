9a241175557ab1978108210bae5cb82186e174c7
karri sowmya <93367820+karrisowmya@users.noreply.github.com>
2021-12-30 20:25:33 +0530

    Update fibonaciiseries.c

--- fibonaciiseries.c
+++ fibonaciiseries.c
@@ -31,4 +31,7 @@ int main() {
 
 /*Output:
 Enter the number of terms: 20
-Fibonacci Series: 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181*/
+Fibonacci Series: 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181
+
+learnings:
+learned how to find fibnocci series*/
