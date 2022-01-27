978b8d830ee11b52ed171ed8af2a127ccd98fb5d
karri sowmya <93367820+karrisowmya@users.noreply.github.com>
2022-01-27 03:38:15 +0000

    array

---
+++ array.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+
+int main(void) {
+  int marks[5];
+  printf("Enter 5 values for marks array...\n");
+  
+  for (int index =0; index < 5; index++)
+    scanf("%d", &marks[index]);
+  
+  printf("The elements entered are...\n");
+  for (int index =0; index < 5; index++)
+    printf("%d\t", marks[index]);
+  return 0;
+}
